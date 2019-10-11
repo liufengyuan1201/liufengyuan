dd5d226fc60cd3fa7b53e83b325ad60633704533
liufengyuan <513937539@qq.com>
2019-10-11 14:04:06 +0800

    第一次提交

---
+++ src/main/java/com/lfy/domain/Test.java
@@ -0,0 +1,13 @@
+package com.lfy.domain;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("查看当前分支:");
+		System.out.println("hello~!");
+		
+		
+		System.out.print("进行修改:");
+		System.out.println("hihi~!");
+	}
+}
